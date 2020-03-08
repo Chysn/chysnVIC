bebc74abc7d0e3e6d30cc70556729d5bcc8d5b9e
Jason Justian <jjustian@gmail.com>
2020-03-07 22:46:31 -0500

    Change to FAC2 method

--- irq-installer/src/irq-installer.asm
+++ irq-installer/src/irq-installer.asm
@@ -20,61 +20,54 @@
 ;        BCC
 ;        BCC CHAIN
 ;
-; IMPORTANT NOTE:
-;    At least one popular assembler assembles the instruction
-;        ADC $00FF,X
-;    using the zeropage,X addressing mode, even though the leading zeroes
-;    should make absolute,X explicit. If the installer crashes your system,
-;    check that.
-;
 * = $1800
 H_IRQ   = $EABF         ; This is the default hardware IRQ
-SCRPAD  = $03           ; Scratchpad memory for self-modifying code location    
+SCRPAD  = $69           ; Scratchpad memory for self-modifying code location    
 IRQV    = $0314         ; Hardware IRQ vector
 
 START:  PHP
         SEI
 
-        LDA SCRPAD      ; Save these scratchpad values so that this start
-        PHA             ; routine is as neutral to its environment as possible
-        LDA SCRPAD + 1
-        PHA
+        LDY #$68        ; Loads routine into FAC2 to populate X with the low
+        STY $69         ; byte and Y with the high byte of the last byte of
+        STY $6B         ; the address of the JSR below.
+        LDY #$AA        ;
+        STY $6A         ; 0069 PLA
+        LDY #$A8        ; 006A TAX
+        STY $6C         ; 006B PLA
+        LDY #$48        ; 006C TAY
+        STY $6D         ; 006D PHA
+        STY $6F         ; 006E TXA
+        LDY #$8A        ; 006F PHA
+        STY $6E         ; 0070 RTS
+        LDY #$60
+        STY $70
+        JSR $0069
         
-        JSR $FFED       ; This is the KERNAL routine SCREEN, which only
-                        ; sets the X and Y registers. It's called only to
-                        ; add a return address to the stack, which will be
-                        ; used to set the location of the IRQ handler.
-        TSX
-        
+        TXA
         CLC
-        LDA #$36        ; The CHAIN vector is 54 ($36) bytes after the JSR,
-        ADC $00FF,X     ; so add 54 to the low byte of the return address and
-        STA SCRPAD      ; stash it for safekeeping.
-        LDA #$00        ; The high byte is only going to change if the
-        ADC $0100,X     ; carry flag is set from the previous ADC
-        STA SCRPAD + 1
+        ADC #$2B        ; The CHAIN vector is 43 ($2B) bytes after the JSR,
+        STA SCRPAD      ; so add 43 to the low byte of the return address
+        TYA
+        ADC #$00        ; The high byte is only going to change if the
+        STA SCRPAD + 1  ; carry flag was set with ADC #$2D above
         
-        LDY #$00        ; The idea here is that multiple IRQ routines can be
-        LDA IRQV        ; installed into memory, with each one "chaining" to
-        STA (SCRPAD),Y  ; the next. This block of code looks up the current IRQ
+        LDY #$00
+        LDA IRQV        ; The idea here is that multple IRA routines can be
+        STA (SCRPAD),Y  ; installed into memory, with each one "chaining" to
+        INY             ; the next. This block of code looks up the current IRQ
         LDA IRQV + 1    ; location, and sets it as the location of the JMP
-        INY             ; at the CHAIN label, below. So your handler should branch to
-        STA (SCRPAD),Y  ; CHAIN when it's finished. Theoretically, it will eventually
-                        ; wind up at the hardware default IRQ handler at $EABF.
-        
+        STA (SCRPAD),Y  ; at the CHAIN label below. Your handler should branch to
+                        ; CHAIN when it's finished.
+ 
         CLC             ; Then, we set the IRQ vector to the new handler,
-        LDA #$02        ; which is two bytes after CHAIN's JMP operand, whose
-        ADC SCRPAD      ; address is still in the scatchpad locations.
-        STA IRQV        ; Set the low byte to the new place
-        LDA #$00        ; The high byte will be the same as it was before
-        ADC SCRPAD + 1  ; unless the carry flag was set in the previous ADC
-        STA IRQV + 1
+        LDA SCRPAD      ; which is two bytes after CHAIN's JMP operand, whose 
+        ADC #$02        ; address is still in the scratchpad locations.
+        STA IRQV        ; Set the low byte of the new handler
+        LDA SCRPAD + 1
+        ADC #$00        ; The high byte will be the same as it was before
+        STA IRQV + 1    ; unless the carry flag was set by ADC #$02
         
-        PLA             ; Restore the scratchpad values as a courtesy to the universe
-        STA SCRPAD + 1
-        PLA
-        STA SCRPAD
-
         PLP
         RTS
         
